8f49e9db8e369eab8f0d825701530c9631fcf70b
Kelvin Jasperson <github@mqp3.com>
2018-07-06 15:28:04 -0600

    update to latest TF version

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
   region     = "us-west-1"
 }
 
@@ -18,6 +13,7 @@ resource "aws_s3_bucket" "www" {
 }
 
 resource "aws_s3_bucket_object" "index" {
+  acl          = "public-read"
   bucket       = "www.zxjinn.com"
   content_type = "text/html"
   depends_on   = ["aws_s3_bucket.www"]
@@ -26,6 +22,7 @@ resource "aws_s3_bucket_object" "index" {
   source       = "site/index.html"
 }
 resource "aws_s3_bucket_object" "keybase" {
+  acl        = "public-read"
   bucket     = "www.zxjinn.com"
   content_type = "text/plain"
   depends_on = ["aws_s3_bucket.www"]
